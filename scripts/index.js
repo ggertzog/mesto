459d3c7a9c140e2dd178785db323c700224d6d6d
ggertzog <musienko.roma@mail.ru>
2023-06-07 15:22:02 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,34 @@
 let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__container');
 let popupButtonOpen = document.querySelector('.profile__edit-button');
 let popupButtonClose = document.querySelector('.popup__button-close');
-let popupButtonSave = document.querySelector('.popup__button-submit');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-about');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_about');
 let userName = document.querySelector('.profile__user-name');
 let userAbout = document.querySelector('.profile__user-description');
 
+//функция открытия поп-апа, присваиваем инпутам текстовые значения
 function openPopupForm (){
     popup.classList.add('popup_opened');
     nameInput.value = userName.textContent
     jobInput.value = userAbout.textContent
 }
 
+//функция закрытия поп-апа
 function closePopupForm (){
     popup.classList.remove('popup_opened');
 }
 
-function savePopupForm () {
-    userName.textContent = nameInput.value
-    userAbout.textContent = jobInput.value
-    popup.classList.remove('popup_opened');
-}
-
-function handleFormSubmit (evt) {
+//функция отправки формы и присвоения текстовых значений
+function savePopupForm (evt) {
     evt.preventDefault();
     userName.textContent = nameInput.value
     userAbout.textContent = jobInput.value
+    closePopupForm();
 }
-
+//слушатель для открытия поп-апа
 popupButtonOpen.addEventListener('click', openPopupForm);
-
+//слушатель закрытия поп-апа
 popupButtonClose.addEventListener('click', closePopupForm);
-
-popupButtonSave.addEventListener('click', savePopupForm);
-
-popup.addEventListener('submit', handleFormSubmit);
+//слушатель отправки формы на сервер
+popupForm.addEventListener('submit', savePopupForm);
